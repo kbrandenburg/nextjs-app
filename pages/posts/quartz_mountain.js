7d2dc0b824fcef00b68bdff3f2ef9efb1c508870
Keith Brandenburg <kab2003@gmail.com>
2021-06-02 12:38:13 -0500

    Update quartz_mountain.js
    I might add vacation pics at one point

--- pages/posts/quartz_mountain.js
+++ pages/posts/quartz_mountain.js
@@ -3,6 +3,30 @@ import Image from 'next/image'
 import Link from 'next/link'
 import Head from 'next/head'
 
+import { getAllPostIds, getPostData } from '../../lib/posts'
+
+import Date from '../../components/date'
+import utilStyles from '../../styles/utils.module.css'
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id)
+  return {
+    props: {
+      postData
+    }
+  }
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+
+
 export default function quartz_mountain() {
   return (
     <Layout>
